54af9738448f2810fa9a8b685c6f03b8fec4e3d8
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-03-16 18:43:18 +0300

    feat: :sparkles: Create an helper that handle unexpected error.

---
+++ utils/errors/handleUnkownError.ts
@@ -0,0 +1,10 @@
+import { ServerError } from "./serverError";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+export const handleUnknownError = (error: unknown): ServerError => {
+  // TODO handle error
+
+  const serverError: ServerError = createServerError();
+
+  return serverError;
+};
