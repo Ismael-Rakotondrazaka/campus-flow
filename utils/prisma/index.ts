94a27c4adf2bb41bd83a459e9ed36347cbe6af30
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-03-16 18:24:44 +0300

    feat: :sparkles: Add computed fullName property to user.

--- utils/prisma/index.ts
+++ utils/prisma/index.ts
@@ -1,6 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma: PrismaClient = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+  result: {
+    user: {
+      // add fullName field to user
+      fullName: {
+        needs: {
+          firstName: true,
+          name: true,
+        },
+        compute(user) {
+          return `${user.name} ${user.firstName}`;
+        },
+      },
+    },
+  },
+});
 
 export const usePrismaClient = () => {
   return prisma;
