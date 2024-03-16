71b054350fee918735b72032d0efea5aafd1adfa
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-03-16 20:01:00 +0300

    feat: :sparkles: Create schemas for login.

---
+++ utils/schemas/login/index.ts
@@ -0,0 +1,31 @@
+import { z } from "#imports";
+
+/* -------------------------------------------------------------------------- */
+/*                                 Login body                                 */
+/* -------------------------------------------------------------------------- */
+
+export const LoginBodySchema = z.object({
+  email: emailSchema,
+  password: passwordSchema,
+});
+
+export type LoginBody = z.infer<typeof LoginBodySchema>;
+
+/* -------------------------------------------------------------------------- */
+/*                                 Login data                                 */
+/* -------------------------------------------------------------------------- */
+
+export type LoginData = {
+  accessToken: AccessToken;
+};
+
+/* -------------------------------------------------------------------------- */
+/*                                 Login error                                */
+/* -------------------------------------------------------------------------- */
+
+// * PEM: Parse Error Message
+export type LoginBodyPEM = RequestErrorMessage<LoginBody>;
+
+export type LoginError = RequestError<LoginBodyPEM>;
+
+export type LoginResponse = RequestResponse<LoginData, LoginBody>;
