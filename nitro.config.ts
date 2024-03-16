05613f54489f4b967cbe92bcb95650665988efc6
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-03-16 18:22:41 +0300

    feat: :sparkles: Define runtime config keys.

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,10 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
+  runtimeConfig: {
+    accessTokenSecret: "",
+    databaseUrl: "",
+    appUrl: "",
+  },
   typescript: {
     strict: true,
     tsConfig: {
@@ -21,5 +26,33 @@ export default defineNitroConfig({
   },
   experimental: {
     asyncContext: true,
+    openAPI: true,
+  },
+  imports: {
+    imports: [
+      {
+        from: "@prisma/client",
+        name: "*",
+        as: "prismaCtx",
+      },
+      {
+        from: "zod-form-data",
+        name: "zfd",
+      },
+      {
+        from: "zod",
+        name: "z",
+      },
+      {
+        from: "is-type-of",
+        name: "default",
+        as: "is",
+      },
+      {
+        from: "http-status-codes",
+        name: "*",
+        as: "httpStatusCodes",
+      },
+    ],
   },
 });
