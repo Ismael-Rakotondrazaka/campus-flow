f9008f041d618a0570d167a1c228dc736c2ff684
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-03-17 19:15:30 +0300

    feat: :sparkles: Add more methods to building repository.
    Add findFullOneOrFail and updateFullOne.

--- utils/repositories/buildings/index.ts
+++ utils/repositories/buildings/index.ts
@@ -1,7 +1,11 @@
 import { findFullOne } from "./findFullOne";
 import { createFullOne } from "./createFullOne";
+import { findFullOneOrFail } from "./findFullOneOrFail";
+import { updateFullOne } from "./updateFullOne";
 
 export const buildingRepository = Object.freeze({
   findFullOne,
   createFullOne,
+  findFullOneOrFail,
+  updateFullOne,
 });
---
+++ utils/repositories/buildings/updateFullOne.ts
@@ -0,0 +1,36 @@
+import { prismaCtx } from "#imports";
+import { BuildingFullRaw, buildingFullRawToBuildingFull } from "./fullRaw";
+
+export const updateFullOne = async ({
+  data,
+  where,
+}: {
+  data: prismaCtx.Prisma.BuildingUpdateArgs["data"];
+  where: prismaCtx.Prisma.BuildingWhereUniqueInput;
+}): Promise<BuildingFull> => {
+  const prismaClient = usePrismaClient();
+
+  const refreshTokenRaw: BuildingFullRaw = await prismaClient.building.update({
+    where,
+    data,
+    include: {
+      lodgments: {
+        // we can either use `include` or `select`, but not both at the same time
+        include: {
+          _count: {
+            select: {
+              students: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          lodgments: true,
+        },
+      },
+    },
+  });
+
+  return buildingFullRawToBuildingFull(refreshTokenRaw);
+};
