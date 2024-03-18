66f05d3478e5f853f54476575283822591ef58e6
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-03-18 16:12:47 +0300

    feat: :sparkles: Add findFullMany to buildingRepository.

---
+++ utils/repositories/buildings/findFullMany.ts
@@ -0,0 +1,42 @@
+import { prismaCtx } from "#imports";
+import { BuildingFullRaw, buildingFullRawToBuildingFull } from "./fullRaw";
+
+export const findFullMany = async (payload: {
+  where?: prismaCtx.Prisma.BuildingWhereInput;
+  orderBy?: prismaCtx.Prisma.BuildingOrderByWithRelationInput;
+  skip?: number;
+  take?: number;
+}): Promise<BuildingFull[]> => {
+  const prismaClient = usePrismaClient();
+
+  const buildingFullRaws: BuildingFullRaw[] =
+    await prismaClient.building.findMany({
+      where: payload.where,
+      orderBy: payload.orderBy,
+      skip: payload.skip,
+      take: payload.take,
+      include: {
+        lodgments: {
+          include: {
+            _count: {
+              select: {
+                students: true,
+              },
+            },
+          },
+        },
+        _count: {
+          select: {
+            lodgments: true,
+          },
+        },
+      },
+    });
+
+  const buildingFulls: BuildingFull[] = buildingFullRaws.map(
+    (buildingFullRaw: BuildingFullRaw) =>
+      buildingFullRawToBuildingFull(buildingFullRaw),
+  );
+
+  return buildingFulls;
+};
