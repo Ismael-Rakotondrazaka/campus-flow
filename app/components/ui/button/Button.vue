2a9fae15dccd2a3145e4739f3c389086eeefba62
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-04-01 20:08:05 +0300

    feat: :sparkles: Add UI components for Avatar, Badge, Button, ButtonGroup, Calendar, and Card
    - Introduce new Vue components for Avatar, Badge, Button, ButtonGroup, Calendar, and Card, enhancing the UI library.
    - Implement various subcomponents for each main component, including AvatarImage, AvatarFallback, CalendarCell, CalendarGrid, and more.
    - Utilize Tailwind CSS for styling and ensure responsive design across components.
    - Establish a consistent API for component props and slots, improving usability and integration within the application.

---
+++ app/components/ui/button/Button.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { PrimitiveProps } from 'reka-ui';
+import type { HTMLAttributes } from 'vue';
+
+import { Primitive } from 'reka-ui';
+
+import { cn } from '@/lib/utils';
+
+import type { ButtonVariants } from '.';
+
+import { buttonVariants } from '.';
+
+interface Props extends PrimitiveProps {
+  class?: HTMLAttributes['class'];
+  size?: ButtonVariants['size'];
+  variant?: ButtonVariants['variant'];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: 'button',
+});
+</script>
+
+<template>
+  <Primitive
+    data-slot="button"
+    :data-variant="variant"
+    :data-size="size"
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+  >
+    <slot />
+  </Primitive>
+</template>
