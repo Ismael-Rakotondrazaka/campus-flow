b999bee7fe6f486aaf772519ce952313e0b408a2
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-04-03 00:17:09 +0300

    feat: :sparkles: Add new layout components for maintenance, renewal, reservation, root, and student pages
    - Introduce Vue components for maintenance, renewal, reservation, root, and student layouts, each featuring a simple template structure with slot support.
    - Enhance the application's layout organization and navigation capabilities by providing dedicated components for different sections.

---
+++ app/layouts/root.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import RootSidebar from '@/features/root/sidebars/components/RootSidebar.vue';
+</script>
+
+<template>
+  <SidebarProvider>
+    <RootSidebar />
+    <main>
+      <SidebarTrigger />
+      <slot />
+    </main>
+  </SidebarProvider>
+</template>
