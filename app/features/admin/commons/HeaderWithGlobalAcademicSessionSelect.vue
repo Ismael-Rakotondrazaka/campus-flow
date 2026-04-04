24fa146aa38d318434f01c9408e5b8fafa3e24d6
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-04-04 11:01:59 +0300

    feat: :sparkles: Add HeaderWithGlobalAcademicSessionSelect component to enhance dashboard layout
    - Introduce HeaderWithGlobalAcademicSessionSelect component for improved organization of academic session selection in the admin dashboard.
    - Refactor dashboard.vue to utilize the new component, streamlining the layout and enhancing user experience.

---
+++ app/features/admin/commons/HeaderWithGlobalAcademicSessionSelect.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import GlobalAcademicSessionSelect from '~/features/shared/academic-sessions/components/GlobalAcademicSessionSelect.vue';
+</script>
+
+<template>
+  <div
+    class="mb-4 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between"
+  >
+    <slot />
+
+    <GlobalAcademicSessionSelect />
+  </div>
+</template>
--- app/pages/admin/root/dashboard.vue
+++ app/pages/admin/root/dashboard.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
+import HeaderWithGlobalAcademicSessionSelect from '~/features/admin/commons/HeaderWithGlobalAcademicSessionSelect.vue';
 import DashboardGreeting from '~/features/admin/dashboard/components/DashboardGreeting.vue';
 import HousingApplicationKPICard from '~/features/admin/dashboard/components/HousingApplicationKPICard.vue';
 import MaintenanceKPICard from '~/features/admin/dashboard/components/MaintenanceKPICard.vue';
 import RenewalKPICard from '~/features/admin/dashboard/components/RenewalKPICard.vue';
 import ResidentKPICard from '~/features/admin/dashboard/components/ResidentKPICard.vue';
 import RecentHousingApplicationsTable from '~/features/admin/housing-applications/RecentHousingApplicationsTable.vue';
-import GlobalAcademicSessionSelect from '~/features/shared/academic-sessions/components/GlobalAcademicSessionSelect.vue';
 </script>
 
 <template>
   <div class="container mx-auto px-2">
-    <div
-      class="mb-4 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between"
-    >
+    <HeaderWithGlobalAcademicSessionSelect>
       <DashboardGreeting />
-      <GlobalAcademicSessionSelect />
-    </div>
+    </HeaderWithGlobalAcademicSessionSelect>
 
     <div class="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
       <HousingApplicationKPICard />
