d57510aa3d64b120611ef1087ed7845063255977
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-04-04 08:00:04 +0300

    style: :art: Adjust padding in Card component styles for improved layout
    - Update padding values in Card, CardContent, CardFooter, and CardHeader components to enhance visual consistency and spacing.
    - Modify class bindings to reflect new padding values, ensuring a cohesive design across card elements.

--- app/components/ui/card/Card.vue
+++ app/components/ui/card/Card.vue
@@ -13,7 +13,7 @@ const props = defineProps<{
     data-slot="card"
     :class="
       cn(
-        'bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm',
+        'bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-4 shadow-sm',
         props.class
       )
     "
--- app/components/ui/card/CardFooter.vue
+++ app/components/ui/card/CardFooter.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
 <template>
   <div
     data-slot="card-footer"
-    :class="cn('flex items-center px-6 [.border-t]:pt-6', props.class)"
+    :class="cn('flex items-center px-4 [.border-t]:pt-4', props.class)"
   >
     <slot />
   </div>
