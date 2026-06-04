a03c39c354fca75766ab3c669fc21a9cd79cba45
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-06-04 20:54:14 +0300

    feat(app): update global middleware
    - Update middleware

--- app/middleware/global-academic-session.global.ts
+++ app/middleware/global-academic-session.global.ts
@@ -6,14 +6,18 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
   const id = toId || fromId;
 
+  const localeRoute = useLocaleRoute();
+
   // Only redirect if the normalized value is different
   if (id && toId !== id) {
-    return navigateTo({
-      path: to.path,
-      query: {
-        ...to.query,
-        g_academic_session_id: id,
-      },
-    });
+    return navigateTo(
+      localeRoute({
+        path: to.path,
+        query: {
+          ...to.query,
+          g_academic_session_id: id,
+        },
+      })
+    );
   }
 });
