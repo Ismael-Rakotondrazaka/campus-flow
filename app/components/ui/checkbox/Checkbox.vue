8f4152aee1db40c50fd809470a49921dddd6d1e3
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-06-04 20:54:02 +0300

    feat(ui): update checkbox UI primitives
    - Update app/components/ui/checkbox

--- app/components/ui/checkbox/Checkbox.vue
+++ app/components/ui/checkbox/Checkbox.vue
@@ -6,7 +6,7 @@ import { reactiveOmit } from '@vueuse/core';
 import { Check } from 'lucide-vue-next';
 import { CheckboxIndicator, CheckboxRoot, useForwardPropsEmits } from 'reka-ui';
 
-import { cn } from '@/lib/utils';
+import { cn } from '~/lib/utils';
 
 const props = defineProps<
   { class?: HTMLAttributes['class'] } & CheckboxRootProps
