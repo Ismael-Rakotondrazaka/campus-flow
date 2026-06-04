69aec0268e7d7f737c0194068a6d44dc8413a188
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-06-04 20:54:06 +0300

    feat(ui): update native-select UI primitives
    - Update app/components/ui/native-select

--- app/components/ui/native-select/NativeSelect.vue
+++ app/components/ui/native-select/NativeSelect.vue
@@ -5,7 +5,7 @@ import type { HTMLAttributes } from 'vue';
 import { reactiveOmit, useVModel } from '@vueuse/core';
 import { ChevronDownIcon } from 'lucide-vue-next';
 
-import { cn } from '@/lib/utils';
+import { cn } from '~/lib/utils';
 
 defineOptions({
   inheritAttrs: false,
--- app/components/ui/native-select/NativeSelectOptGroup.vue
+++ app/components/ui/native-select/NativeSelectOptGroup.vue
@@ -1,10 +1,9 @@
 <!-- @fallthroughAttributes true -->
-<!-- @strictTemplates true -->
 
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue';
 
-import { cn } from '@/lib/utils';
+import { cn } from '~/lib/utils';
 
 const props = defineProps<{ class?: HTMLAttributes['class'] }>();
 </script>
