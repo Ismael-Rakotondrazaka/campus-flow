99a9c1c88718580bc81d82f88a16d14ac32ce774
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-06-04 20:54:10 +0300

    feat(ui): update spinner UI primitives
    - Update app/components/ui/spinner

--- app/components/ui/spinner/Spinner.vue
+++ app/components/ui/spinner/Spinner.vue
@@ -3,7 +3,7 @@ import type { HTMLAttributes } from 'vue';
 
 import { Loader2Icon } from 'lucide-vue-next';
 
-import { cn } from '@/lib/utils';
+import { cn } from '~/lib/utils';
 
 const props = defineProps<{
   class?: HTMLAttributes['class'];
