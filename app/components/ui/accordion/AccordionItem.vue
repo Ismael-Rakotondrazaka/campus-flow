5013650fb90267451a10b22baed1ff9b96d85331
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-06-04 20:53:59 +0300

    feat(ui): update accordion UI primitives
    - Update app/components/ui/accordion

---
+++ app/components/ui/accordion/AccordionItem.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { AccordionItemProps } from 'reka-ui';
+import type { HTMLAttributes } from 'vue';
+
+import { reactiveOmit } from '@vueuse/core';
+import { AccordionItem, useForwardProps } from 'reka-ui';
+
+import { cn } from '~/lib/utils';
+
+const props = defineProps<
+  { class?: HTMLAttributes['class'] } & AccordionItemProps
+>();
+
+const delegatedProps = reactiveOmit(props, 'class');
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <AccordionItem
+    v-slot="slotProps"
+    data-slot="accordion-item"
+    v-bind="forwardedProps"
+    :class="cn('border-b last:border-b-0', props.class)"
+  >
+    <slot v-bind="slotProps" />
+  </AccordionItem>
+</template>
