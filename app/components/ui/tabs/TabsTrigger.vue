13696d1dc0c8cb6c59c3b6022cf9b7610e2a159c
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-06-04 20:54:12 +0300

    feat(ui): update tabs UI primitives
    - Update app/components/ui/tabs

--- app/components/ui/tabs/TabsTrigger.vue
+++ app/components/ui/tabs/TabsTrigger.vue
@@ -5,7 +5,7 @@ import type { HTMLAttributes } from 'vue';
 import { reactiveOmit } from '@vueuse/core';
 import { TabsTrigger, useForwardProps } from 'reka-ui';
 
-import { cn } from '@/lib/utils';
+import { cn } from '~/lib/utils';
 
 const props = defineProps<
   { class?: HTMLAttributes['class'] } & TabsTriggerProps
