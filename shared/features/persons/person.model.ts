fff5479273e3f653dda2fac1bff90eadc9180b22
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-06-04 20:53:16 +0300

    feat(shared): add shared contracts for persons
    - Add shared/features/persons

---
+++ shared/features/persons/person.model.ts
@@ -0,0 +1,23 @@
+import { createEnumConstants } from '../../utils/enums';
+
+export const Genders = ['male', 'female'] as const;
+
+export const Gender = createEnumConstants(Genders);
+
+export type Gender = (typeof Gender)[keyof typeof Gender];
+
+export const GenderLabel: Record<Gender, string> = {
+  [Gender.female]: 'Femme',
+  [Gender.male]: 'Homme',
+};
+
+export const Origins = ['national', 'foreigner'] as const;
+
+export const Origin = createEnumConstants(Origins);
+
+export type Origin = (typeof Origin)[keyof typeof Origin];
+
+export const OriginLabel: Record<Origin, string> = {
+  [Origin.foreigner]: 'Étranger',
+  [Origin.national]: 'National',
+};
