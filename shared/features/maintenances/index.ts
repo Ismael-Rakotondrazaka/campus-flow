fb7dda269d6c9df8e9467041900507d0f5ee97fc
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-06-04 20:53:15 +0300

    feat(shared): add shared contracts for maintenances
    - Add shared/features/maintenances

---
+++ shared/features/maintenances/index.ts
@@ -0,0 +1,5 @@
+export * from './maintenance.ability';
+export * from './maintenance.config';
+export * from './maintenance.model';
+export * from './maintenance.requests';
+export * from './maintenance.schema';
