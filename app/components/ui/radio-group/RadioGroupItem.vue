eca847e200db3c1c5c78f9542509634fed0fb4e1
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-06-04 20:54:08 +0300

    feat(ui): update radio-group UI primitives
    - Update app/components/ui/radio-group

--- app/components/ui/radio-group/RadioGroupItem.vue
+++ app/components/ui/radio-group/RadioGroupItem.vue
@@ -6,7 +6,7 @@ import { reactiveOmit } from '@vueuse/core';
 import { CircleIcon } from 'lucide-vue-next';
 import { RadioGroupIndicator, RadioGroupItem, useForwardProps } from 'reka-ui';
 
-import { cn } from '@/lib/utils';
+import { cn } from '~/lib/utils';
 
 const props = defineProps<
   { class?: HTMLAttributes['class'] } & RadioGroupItemProps
