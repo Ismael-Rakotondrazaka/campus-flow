0d77277ee2576f00af2e276eaadee49514e7e50b
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2026-06-04 20:54:11 +0300

    feat(ui): update stepper UI primitives
    - Update app/components/ui/stepper

--- app/components/ui/stepper/StepperTitle.vue
+++ app/components/ui/stepper/StepperTitle.vue
@@ -5,7 +5,7 @@ import type { HTMLAttributes } from 'vue';
 import { reactiveOmit } from '@vueuse/core';
 import { StepperTitle, useForwardProps } from 'reka-ui';
 
-import { cn } from '@/lib/utils';
+import { cn } from '~/lib/utils';
 
 const props = defineProps<
   { class?: HTMLAttributes['class'] } & StepperTitleProps
